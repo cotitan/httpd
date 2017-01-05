06b3f2a152f9782e9f6b1996760544c4cafdb84e
cotitan <cotitan@outlook.com>
2017-01-06 00:14:15 +0800

    thread-pool test

--- test/main.cpp
+++ test/main.cpp
@@ -3,8 +3,10 @@
 int main() {
 	int nThr = 10;
 	int test::nThread = nThr;
-	pthread_mutex_init(&(int test::mutex), NULL);
-	sem_init(&(int test::nJobs), 0, 0);
+	sem_t test::nJobs;
+	pthread_t test::mutex;
+	pthread_mutex_init(&(test::mutex), NULL);
+	sem_init(&(test::nJobs), 0, 0);
 	pthread_t* test::threads = new pthread_t[nThr];
 	test test1;
 	for (int i = 0; i < nThr; i++)
