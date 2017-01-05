e036b955801e282de5edc9f26861b49dcfc1a14b
cotitan <cotitan@outlook.com>
2017-01-05 23:38:25 +0800

    thread-pool test

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,13 @@
 #include "test.h"
 
 int main() {
+	int nThr = 10;
 	int test::nThread = nThr;
 	pthread_mutex_init(&(int test::mutex), NULL);
 	sem_init(&(int test::nJobs), 0, 0);
 	pthread_t* test::threads = new pthread_t[nThr];
 	test test1;
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < nThr; i++)
 		test1.add_job(i);
 	return 0;
 }
