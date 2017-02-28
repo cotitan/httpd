89394f966853895aa26d6d10e7a4b1dd7b2d8a9b
cotitan <cotitan@outlook.com>
2017-02-28 14:56:22 +0800

    pack read and write

--- include/my_io.h
+++ include/my_io.h
@@ -1,9 +1,9 @@
 #ifndef MY_IO_H
 #define MY_IO_H
 
-ssize_t readn(int fd, void * data, int len);
+int readn(int fd, void * data, int len);
 
-ssize_t writen(int fd, const void *data, size_t n);
+int writen(int fd, const void *data, size_t n);
 
 
 #endif
